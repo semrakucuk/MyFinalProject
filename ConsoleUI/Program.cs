6353cdfcc2c854595ce17a41b2669f29941c14dc
semra küçük <semrakucuk97@gmail.com>
2021-02-06 23:24:22 +0300

    06.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            // CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach(var product in productManager.GetByUnitPrice(10,40))
+            foreach (var product in productManager.GetByUnitPrice(10, 40))
             {
                 Console.WriteLine(product.ProductName);
 
             }
-
         }
     }
 }
